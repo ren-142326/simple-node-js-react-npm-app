4a7afae62908fb1d33349343df4a7b0ca91fe94a
renjw <2909001175@qq.com>
2024-09-20 11:02:07 +0800

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
 import React, {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import createRoot from 'react-dom/clinet';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.createRoot(div).render(
+  createRoot(div).render(
     <StrictMode>
       <App />
     </StrictMode>
