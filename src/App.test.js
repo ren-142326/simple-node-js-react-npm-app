d7f0e502494823e44cee344236f32e6f72fc169f
renjw <2909001175@qq.com>
2024-09-20 11:05:50 +0800

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import React, {StrictMode} from 'react';
-import createRoot from 'react-dom/clinet';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
